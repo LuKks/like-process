01a9313801def5449cffda998a0366f4834b844f
LuKks <lucasbarrena@hotmail.com>
2019-07-20 06:32:00 -0300

    first pre-release

---
+++ README.md
@@ -0,0 +1,137 @@
+# like-process
+
+Handle signals for gracefully exit with 100% uptime (cluster compatible)
+
+![](https://img.shields.io/npm/v/like-process.svg) [![](https://img.shields.io/maintenance/yes/2019.svg?style=flat-square)](https://github.com/LuKks/like-process) ![](https://img.shields.io/github/size/lukks/like-process/index.js.svg) ![](https://img.shields.io/npm/dt/like-process.svg) ![](https://img.shields.io/github/license/LuKks/like-process.svg)
+
+### I'm still working on it.
+
+I made this pre-release to use on my project so it's useful,\
+but I want that the library handles all the cases easily.
+I need more time to do some changes, remove useless code, etc.
+
+```javascript
+const like = require('like-process');
+
+if(like.isMaster) {
+  console.log('master', process.pid);
+  like.fork();
+  return;
+}
+
+console.log('started', process.pid);
+
+const app = require('express')();
+
+app.get('/', (req, res) => {
+  res.send('short [' + process.pid + ']');
+});
+
+app.get('/long', (req, res) => {
+  setTimeout(() => res.send('long [' + process.pid + ']'), 5000);
+});
+
+const serverA = app.listen(3000, () => console.log('listening', process.pid));
+
+//error in 1ms and still 100% uptime, even with only 1 worker
+setTimeout(() => {
+  this_var_not_exists; //will reload if you are clustering otherwise exit
+
+  //like.exit(1); //exit with optional exitCode
+  //like.reload(); //reload, also can set an exitCode
+
+  //process.kill(process.pid, 'SIGTERM'); //exit by signal
+  //process.kill(process.pid, 'SIGHUP'); //reload by signal
+
+  //process.exit(); //the cleanup can't be async with it
+}, 1);
+
+//when you try it, you will see a lot of logs
+//just go to http://localhost:3000 and it's up 100%
+//wait some seconds and try another request, the pid changed
+//it's due the keep-alive, for that you can use like-server
+//but means that it's working
+
+like.on('terminate', () => {
+  //at this moment like.terminated it's and keep as true
+  console.log('terminate', process.pid);
+
+  //commonly you don't need this event but there is some cases
+  //anyway, for example, you can implement a timeout
+});
+
+like.on('cleanup', () => {
+  //at this moment like.cleanup it's and keep as true
+  console.log('cleanup', process.pid); //close db, etc
+  setTimeout(() => console.log('cleanup async', process.pid), 250);
+
+  //if your app run extra code outside the server like setInterval/setTimeout
+  //can use like.terminated or like.cleanup, 'terminate' or 'cleanup' event
+  //to know when to clear in the same code
+});
+
+//for this example the 'disconnect' and 'beforeExit' are not needed
+//but on this way, this same example it's useful for several cases
+like.handle([serverA, 'disconnect', 'uncaughtException', 'beforeExit', 'exit'], (evt, arg1) => {
+  //console.log('handle', evt);
+
+  if(evt === 'uncaughtException') {
+    process.stderr.write(arg1/*err*/.stack + '\n');
+  }
+
+  if(evt === 'exit') {
+    console.log('exited (' + arg1/*code*/ + ')', process.pid);
+  }
+});
+```
+
+## Install
+```
+npm i like-process
+```
+
+#### Handles signals by default:
+- Exit with SIGTERM: swarm, k8s, systemd, etc.
+- Exit with SIGINT: pm2.
+- Reload with SIGHUP: custom, systemd, etc.
+
+Also can send a signal to specific worker or single process.\
+When you do a reload in single process it's equal than doing an exit,\
+otherwise will fork a new worker and when it's ready disconnect the old one.
+
+#### Optionally handle resources (interval, timeout, etc) with:
+- `'terminate'` and `'cleanup'` event.
+- `like.terminated` and `like.cleanup` states.
+
+`terminate`: process want to exit due signal, uncaught ex., like.exit/reload(), etc.\
+`cleanup`: servers closed, worker disconnected, beforeExit or exit (whatever comes first).
+
+#### Compatible:
+- Single process with replicated containers.
+- Single process with pm2 cluster.
+- Worker/s with cluster module.
+
+#### Description
+It was made to combine with [like-server](https://www.npmjs.com/like-server).
+Extremely useful when you have deployment with Docker, pm2, k8s, etc.\
+Should be enough for all the cases using the different events and states.\
+Async cleanup when it's possible.\
+Using pm2 will send the ready signal when all servers are listening.\
+Using cluster module there is also an internal ready signal.
+
+## Examples
+It's a pre-release, the usage can change and there is a lot of cases,\
+so I did not write more examples for this moment.\
+\
+I think that only the like.handle() can change a bit\
+because I don't like to force the stderr.write manually.\
+Also can be problems to easily attach the server object.\
+And I need more tests to know that it's apply perfect for all the cases.
+
+## Tests
+```
+There are no tests yet
+```
+
+## License
+Code released under the [MIT License](https://github.com/LuKks/like-process/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,259 @@
+/*
+ like-process (https://npmjs.com/package/like-process)
+ Copyright 2019 Lucas Barrena
+ Licensed under MIT (https://github.com/LuKks/like-process)
+*/
+
+'use strict';
+
+let like = new (require('events'))();
+
+like.terminated = false;
+like.cleanup = false;
+
+const debug = require('debug');
+const log = debug('like-process');
+//debug.enable('like-process');
+
+const cluster = require('cluster');
+like.isCluster = cluster.isWorker; //is worker or master with at least 1 fork
+like.isMaster = cluster.isMaster;
+like.isWorker = cluster.isWorker;
+
+like.cluster = function(count, env) {
+  if(cluster.isMaster) {
+    return;
+  }
+
+  count = count === true ? require('os').cpus().length : (Number(count) || 1);
+
+  for(let i = 0; i < count; i++) {
+    like.fork(env);
+  }
+
+  return true;
+}
+
+like.fork = function(env) {
+  if(!cluster.isMaster) {
+    return;
+  }
+
+  let worker = cluster.fork(env);
+  worker.$env = env;
+
+  worker.process.on('internalMessage', function onTerminate(msg) {
+    if(msg.cmd === 'NODE_LIKE_PROCESS' && (msg.action === 'exit' || msg.action === 'reload')) {
+      log('worker want to', msg.action);
+
+      //worker.process.removeListener('internalMessage', onTerminate);
+      like[msg.action](undefined, worker);
+    }
+  });
+
+  like.isCluster = true;
+
+  return worker;
+}
+
+like.exit = terminate.bind(null, 'exit');
+like.reload = terminate.bind(null, 'reload');
+
+function terminate(action, code, worker) {
+  worker = typeof worker === 'object' ? worker : undefined;
+
+  //temp fix; I don't like when I do a ctrl+c and doesn't exit instantly
+  //but SIGINT it's for pm2 so we only check if process.send not exists
+  if(code/*signal*/ === 'SIGINT' && !process.send) {
+    return process.exit(1);
+  }
+
+  //single process or worker
+  if(!like.isCluster || cluster.isWorker) {
+    if(isFinite(code)) {
+      process.exitCode = code;
+    }
+  }
+
+  //avoid multiple executions
+  if(like.terminated || worker && worker.$terminated) {
+    log('terminate: called again', action);
+    return;
+  }
+
+  //single process or worker
+  if(!like.isCluster || cluster.isWorker) {
+    log('terminate: saving process/worker action', action);
+
+    like.terminated = true;
+    like.emit('terminate');
+  }
+  //master to specific worker object
+  else if(worker) {
+    log('terminate: saving master-worker action', action);
+
+    worker.$terminated = true;
+    like.emit('terminate', worker);
+  }
+
+  //single process
+  if(!like.isCluster) {
+    log('is not cluster', action, code);
+    return exit();
+  }
+  //master
+  else if(cluster.isMaster) {
+    if(worker) {
+      log('is master to worker', action, code);
+      return action === 'exit' ? exit(worker) : reload(worker);
+    }
+    else {
+      log('is master to all', action, code);
+
+      for(let id in cluster.workers) {
+        log('master to worker with', action, code);
+        terminate(action, code, cluster.workers[id]);
+      }
+    }
+  }
+  //worker
+  else if(cluster.isWorker) {
+    log('is worker', action, code);
+    process.send({ cmd: 'NODE_LIKE_PROCESS', action });
+  }
+
+  return true;
+}
+
+let servers = [];
+
+function exit(worker) {
+  log('exit process', worker ? worker.process.pid : process.pid);
+
+  //master to specific worker object
+  if(worker) {
+    log('disconnecting worker');
+    return worker.disconnect();
+  }
+  //single process
+  else {
+    for(let i = 0; i < servers.length; i++) {
+      servers[i].close();
+    }
+  }
+}
+
+function reload(worker) {
+  log('reload process', process.pid);
+
+  let newer = like.fork(worker.$env);
+
+  newer.process.on('internalMessage', function onReady(msg) {
+    if(msg.cmd === 'NODE_LIKE_PROCESS' && msg.action === 'ready') {
+      log('newer started, disconnecting old worker');
+
+      newer.process.removeListener('internalMessage', onReady);
+      exit(worker);
+    }
+  });
+}
+
+like.ready = function() {
+  log('its ready', process.pid);
+  process.send && process.send(cluster.isWorker ? { cmd: 'NODE_LIKE_PROCESS', action: 'ready' } : 'ready');
+}
+
+let wait_listening = false;
+let immediateReady;
+
+like.handle = function(events, callback) {
+  if(!callback) {
+    callback = (evt, arg1) => {};
+  }
+
+  log('handle', cluster.isMaster ? 'master' : 'worker', events.map(v => typeof v !== 'object' ? v : 'server'));
+
+  for(let k in events) {
+    //server/s object (multi listening ready and server close event)
+    if(typeof events[k] === 'object') {
+      let server = events[k];
+
+      if(servers.indexOf(server) === -1) {
+        log('server push');
+        servers.push(server);
+
+        if(!server.listening) {
+          log('waiting for listening');
+          wait_listening = true;
+          server.once('listening', multi_listening_ready);
+        }
+      }
+
+      server.once('close', handler.bind(null, callback, 'server', server));
+    }
+    //process event
+    else {
+      process.on(events[k], handler.bind(null, callback, events[k]));
+    }
+  }
+
+  _ready(!wait_listening);
+}
+
+function handler(callback, event, arg1, arg2) {
+  log('event', typeof event === 'object' ? 'its server object' : event);
+
+  //server closed
+  if(event === 'server') {
+    servers.splice(servers.indexOf(arg1), 1);
+  }
+  
+  //wasn't manually, for example, was an uncaught exception
+  if(!like.terminated) {
+    log('wasnt manually');
+    !like.isCluster ? like.exit() : like.reload();
+  }
+
+  //the first event without servers listening or forced exit will turn cleanup
+  if(!like.cleanup && (!servers.length || event === 'exit')) {
+    log('turning cleanup');
+
+    like.cleanup = true;
+    like.emit('cleanup');
+  }
+
+  //
+  callback.call(null, event, arg1, arg2);
+}
+
+function multi_listening_ready() {
+  for(let i = 0; i < servers.length; i++) {
+    if(!servers[i].listening) {
+      return;
+    }
+  }
+
+  log('all servers listening');
+  _ready(true);
+}
+
+function _ready(set) {
+  clearImmediate(immediateReady);
+
+  if(set) {
+    immediateReady = setImmediate(like.ready);
+  }
+}
+
+process.once('SIGTERM', like.exit); //swarm, k8s, systemd, etc
+process.once('SIGINT', like.exit); //pm2
+process.once('SIGHUP', like.reload); //systemd, custom, etc
+
+/*
+disable signal:
+process.removeListener('SIGTERM', like.exit);
+process.removeListener('SIGINT', like.exit);
+process.removeListener('SIGUSR2', like.reload);
+*/
+
+module.exports = like;
